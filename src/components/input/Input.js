617e16e053454384a3bdc63791a1e85d3faec22f
seif0912 <baddredineseif@gmail.com>
2023-02-17 13:01:34 +0100

    retouch

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -4,24 +4,15 @@ import {  db } from '../../firebase'
 import { addDoc, collection } from "firebase/firestore";
 import { useAuth } from '../../contexts/AuthContext'
 
-const Input = ({list, setList}) => {
+const Input = () => {
     const {currentUser} = useAuth()
     
-    const idGenerator = (list) =>{
-        if (list.length === 0)
-            return 0
-        let max = list[0].id;
-        for(let i=1; i<list.length; i++)
-            max = max < list[i].id && list[i].id
-        return (max + 1)
-    }
+    
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        let id = idGenerator(list);
-        const task = {task: e.target.task.value, isDone: false, id}
-        const taskk = {task: e.target.task.value, isDone: false}
-        const docRef = await addDoc(collection(db, 'users', currentUser.uid, 'todos'), taskk)
+        const task = {task: e.target.task.value, isDone: false}
+        const docRef = await addDoc(collection(db, 'users', currentUser.uid, 'todos'), task)
         console.log(docRef)
         // setList(prev => [...prev, task])
         e.target.task.value = ''
